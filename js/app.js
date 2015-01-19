48fb6648f47703cd758df6cc6a1035a6e3ff595d
Gerard Braad <me@gbraad.nl>
2015-01-19 17:26:00 +0800

    Add life total reset function

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
 window.addEventListener("load", function() {
   var d = document;
   var players = d.getElementsByClassName("player");
- 
+  var startingLifeTotal = parseInt(players[0].getElementsByClassName("player-life-total")[0].innerHTML);
+
   var playerLifeUp = function() {
     var playerLifeTotalElem = this.parentNode.getElementsByClassName("player-life-total")[0];
     playerLifeTotalElem.innerHTML = parseInt(playerLifeTotalElem.innerHTML) + 1;
-  }
+  };
 
   var playerLifeDown = function() {
     var playerLifeTotalElem = this.parentNode.getElementsByClassName("player-life-total")[0];
     playerLifeTotalElem.innerHTML = parseInt(playerLifeTotalElem.innerHTML) - 1;
-  }
+  };
+
+  var playerLifeReset = function() {
+    var playerLifeTotalElem = this.parentNode.getElementsByClassName("player-life-total")[0];
+    playerLifeTotalElem.innerHTML = startingLifeTotal;
+  };
 
   for (var i = 0; i < players.length; i++) {
     var lifeUp = players[i].getElementsByClassName("player-life-up")[0];
     lifeUp.addEventListener("click", playerLifeUp);
     var lifeDown = players[i].getElementsByClassName("player-life-down")[0];
     lifeDown.addEventListener("click", playerLifeDown);
+    var lifeTotal = players[i].getElementsByClassName("player-life-total")[0];
+    lifeTotal.addEventListener("click", playerLifeReset);
   }
 
 });
